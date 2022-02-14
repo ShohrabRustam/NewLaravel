465efa7e15a8c04dbd1691f567e5edc60f997e62
Shohrab Rustam <mohdrustam001@gmail.com>
2022-02-15 01:20:52 +0530

    controller

--- app/Http/Controllers/MyController.php
+++ app/Http/Controllers/MyController.php
@@ -6,5 +6,31 @@ use Illuminate\Http\Request;
 
 class MyController extends Controller
 {
-    //
+      public function index(){
+          return view('welcome');
+      }
+
+      public function about(){
+          return view('about');
+      }
+
+      public function creater(){
+        return env('CREATER_NAME');
+      }
+
+      public function database(){
+        return env('DB_DATABASE');
+      }
+
+      public function users(){
+        return response()->json([
+                    'name' => "Rustam",
+                    'companyt'=>'mPokket'
+                ]);
+      }
+
+      public function redirectURL(){
+          echo "Redirect to the User Url <br>";
+          return redirect('/users');
+      }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,32 +16,46 @@ use App\Http\Controllers\MyController;
 */
 
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
 
-});
+// });
 
-Route::get('/about', function () {
-    return view('about');
+Route::get('/',[MyController::class,'index']);
 
-});
-Route::get('/creater', function () {
-    return env('CREATER_NAME');
 
-});
+// Route::get('/about', function () {
+//     return view('about');
 
-Route::get('/database', function () {
-    return env('DB_DATABASE');
+// });
 
-});
+Route::get('about',[MyController::class,'about']);
 
-Route::get('/users', function (){
-    return response()->json([
-        'name' => "Rustam",
-        'companyt'=>'mPokket'
-    ]);
-});
+// Route::get('/creater', function () {
+//     return env('CREATER_NAME');
 
-Route::get("/name",function(){
-    return redirect('/users');
-});
+// });
+
+Route::get('creater',[MyController::class,'creater']);
+
+// Route::get('/database', function () {
+//     return env('DB_DATABASE');
+
+// });
+
+Route::get('database',[MyController::class,'database']);
+
+// Route::get('/users', function (){
+//     return response()->json([
+//         'name' => "Rustam",
+//         'companyt'=>'mPokket'
+//     ]);
+// });
+
+Route::get('users',[MyController::class,'users']);
+
+// Route::get("/name",function(){
+//     return redirect('/users');
+// });
+
+Route::get('redirect',[MyController::class,'redirectURL']);
