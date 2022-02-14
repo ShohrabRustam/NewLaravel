ce74446189dae71f6d3914d3b74ea386ae6a17b7
Shohrab Rustam <mohdrustam001@gmail.com>
2022-02-14 23:55:40 +0530

    new video laravel

---
+++ resources/views/about.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Laravel</title>
+</head>
+<body>
+    <h3>About Laravel </h3>
+    <ul>
+        <li>-Open-source PHP Framework</li>
+        <li>-MVC Design Pattern </li>
+        <li>-Difference with PHP </li>
+        <li>-Makes it easier to create projects</li>
+    </ul>
+     <h2>Advantages of Laravel </h2>
+     <ul>
+         <li>Authentication </li>
+         <li>Simple API drivers</li>
+         <li>Makes your app faster </li>
+         <li>Helps for security </li>
+     </ul>
+    <h2>Disadvantage Laravel</h2>
+    <ul>
+        <li>Composer
+            <ul>
+
+                <li>Good to use, but not as good as NPM</li>
+            </ul>
+        </li>
+        <li>LESS in-BUILT SUPPORT
+            <ul>
+
+                <li>Bad support compared to Django and Ruby</li>
+            </ul>
+        </li>
+        <li>BAD UPDATE 
+            <ul>
+                <li>Updata might be problematic</li>
+            </ul>
+        </li>
+    </ul>
+    <h2>What can we built in laravel </h2>
+    <ul>
+        <li>ToDo List</li>
+        <li>Login System </li>
+        <li>Blog</li>
+        <li>Bookmarker Management</li>
+        <li>TimeSheet</li>
+        <li>Order App</li>
+        <li>Portfolio</li>
+        <li>Social Network</li>
+        <li>Ecommerce</li>
+    </ul>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+Route::get('/', function () {
+    return view('welcome');
+
+});
+
+Route::get('/about', function () {
+    return view('about');
+
+});
+Route::get('/creater', function () {
+    return env('CREATER_NAME');
+
+});
+
+Route::get('/database', function () {
+    return env('DB_DATABASE');
+
+});
+
+Route::get('/users', function (){
+    return response()->json([
+        'name' => "Rustam",
+        'companyt'=>'mPokket'
+    ]);
+});
+
+Route::get("/name",function(){
+    return redirect('/users');
+});
